b79b5cbd0e835ff2379c335181395b32e9e52e7b
Fabian Vogelsteller <fabian@frozeman.de>
2015-05-15 18:19:41 +0200

    fixed dapp identicon tiny size

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ethereum:elements',
   summary: 'Basic elements for Dapps',
-  version: '0.1.4',
+  version: '0.1.5',
   git: 'http://github.com/ethereum/meteor-package-elements'
 });
 
