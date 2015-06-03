b4de3616ee0332025bd38308973541a065c5f337
Fabian Vogelsteller <fabian@frozeman.de>
2015-06-03 15:20:31 +0200

    add caching for identicon

--- identicon.js
+++ identicon.js
@@ -11,6 +11,12 @@ Return the user identity icon
 @constructor
 */
 
+/**
+The cached identicons
+
+@property cache
+*/
+var cache = {};
 
 Template['dapp_identicon'].helpers({
     /**
@@ -30,4 +36,23 @@ Template['dapp_identicon'].helpers({
 // console.log(returnValue);
 //         return returnValue;
 //     }
+    /**
+    Return the cached or generated identicon
+    
+    @method (identiconData)
+    */
+    'identiconData': function(identity){
+
+        // remove items if the cache is larger than 50 entries
+        if(_.size(cache) > 50) {
+            delete cache[Object.keys(cache)[0]];
+        }
+
+        return cache['ID_'+ identity] || (cache['ID_'+ identity] =  blockies.create({
+            seed: identity,
+            size: 8,
+            scale: 8,
+            bgcolor: '#eee'
+        }).toDataURL());
+    }
 });
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ethereum:elements',
   summary: 'Basic elements for Dapps',
-  version: '0.1.8',
+  version: '0.1.9',
   git: 'http://github.com/ethereum/meteor-package-elements'
 });
 
