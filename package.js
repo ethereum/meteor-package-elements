fdee58ffbf7b3f32b1581165804ea115ccec529c
Fabian Vogelsteller <fabian@frozeman.de>
2015-08-14 18:25:47 +0200

    add better screenshots

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ethereum:elements',
   summary: 'Basic elements for Dapps',
-  version: '0.4.0',
+  version: '0.4.1',
   git: 'http://github.com/ethereum/meteor-package-elements'
 });
 
