753a591b238bc151137f0b40dfd91b5c067d1e1b
Alexandre Van de Sande <alex.vandesande@ethdev.com>
2015-10-13 16:49:53 -0300

    fixed version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ethereum:elements',
   summary: 'Basic elements for Dapps',
-  version: '0.5.3',
+  version: '0.5.2',
   git: 'http://github.com/ethereum/meteor-package-elements'
 });
 
