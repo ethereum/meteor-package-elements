e40c7ff58ca7510f3e3d860e7e1f4753caedd927
Fabian Vogelsteller <fabian@frozeman.de>
2015-11-05 11:52:12 +0100

    updated dependecies version

--- package.js
+++ package.js
@@ -19,12 +19,13 @@ Package.onUse(function(api) {
   // api.use('underscorestring:underscore.string@3.1.1', 'client');
 
   api.use('ethereum:web3@0.15.1', 'client');
-  api.use('ethereum:tools@0.2.0', 'client');
-  api.imply('ethereum:tools@0.2.0', 'client');
+  api.use('ethereum:tools@0.2.95', 'client');
   api.use('frozeman:animation-helper@0.2.5', 'client');
   api.use('frozeman:storage@0.1.8', 'client');
-  api.use('frozeman:template-var@1.1.1', 'client');
-  api.imply('frozeman:template-var@1.1.1', 'client');
+  api.use('frozeman:template-var@1.2.2', 'client');
+  
+  // provide packages for the app developer as well
+  api.imply(['frozeman:template-var','ethereum:tools'], 'client');
 
 
   api.export(['EthElements'], 'client');
