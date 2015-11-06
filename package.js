b059f7fa7d4802626b99dbf0478ebe02243f76c0
Fabian Vogelsteller <fabian@frozeman.de>
2015-11-06 16:08:19 +0100

    lineheight fix in select accounts

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ethereum:elements',
   summary: 'Basic elements for Dapps',
-  version: '0.6.3',
+  version: '0.6.4',
   git: 'http://github.com/ethereum/meteor-package-elements'
 });
 
