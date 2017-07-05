8a90d74d6e6a03c591cb6e0217bf86831b689a32
Alexandre Van de Sande <alex.vandesande@ethdev.com>
2017-07-05 15:52:29 -0300

    add ens support

--- addressInput.js
+++ addressInput.js
@@ -1,8 +1,57 @@
+
 /**
 Template Controllers
 
 @module Templates
 */
+
+var sha3 = (str, opt) => '0x' + web3.sha3(str, opt).replace('0x','');
+
+function namehash(name) {
+    var node = '0x0000000000000000000000000000000000000000000000000000000000000000';
+    if (name != '') {
+        var labels = name.split(".");
+        for(var i = labels.length - 1; i >= 0; i--) {
+            node = sha3(node + sha3(labels[i]).slice(2), {encoding: 'hex'});
+        }
+    }
+    return node.toString();
+}
+
+var ensContract = web3.eth.contract([{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"resolver","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"label","type":"bytes32"},{"name":"owner","type":"address"}],"name":"setSubnodeOwner","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"ttl","type":"uint64"}],"name":"setTTL","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"ttl","outputs":[{"name":"","type":"uint64"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"resolver","type":"address"}],"name":"setResolver","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"owner","type":"address"}],"name":"setOwner","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":false,"name":"owner","type":"address"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":true,"name":"label","type":"bytes32"},{"indexed":false,"name":"owner","type":"address"}],"name":"NewOwner","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":false,"name":"resolver","type":"address"}],"name":"NewResolver","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":false,"name":"ttl","type":"uint64"}],"name":"NewTTL","type":"event"}]);
+var ens = ensContract.at('0x314159265dd8dbb310642f98f50c066173c1259b');
+
+var resolverContract = web3.eth.contract([{"constant":true,"inputs":[{"name":"interfaceID","type":"bytes4"}],"name":"supportsInterface","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"},{"name":"contentTypes","type":"uint256"}],"name":"ABI","outputs":[{"name":"contentType","type":"uint256"},{"name":"data","type":"bytes"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"x","type":"bytes32"},{"name":"y","type":"bytes32"}],"name":"setPubkey","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"content","outputs":[{"name":"ret","type":"bytes32"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"addr","outputs":[{"name":"ret","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"contentType","type":"uint256"},{"name":"data","type":"bytes"}],"name":"setABI","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"name","outputs":[{"name":"ret","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"name","type":"string"}],"name":"setName","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"hash","type":"bytes32"}],"name":"setContent","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"pubkey","outputs":[{"name":"x","type":"bytes32"},{"name":"y","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"addr","type":"address"}],"name":"setAddr","outputs":[],"payable":false,"type":"function"},{"inputs":[{"name":"ensAddr","type":"address"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":false,"name":"a","type":"address"}],"name":"AddrChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":false,"name":"hash","type":"bytes32"}],"name":"ContentChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":false,"name":"name","type":"string"}],"name":"NameChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":true,"name":"contentType","type":"uint256"}],"name":"ABIChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":false,"name":"x","type":"bytes32"},{"indexed":false,"name":"y","type":"bytes32"}],"name":"PubkeyChanged","type":"event"}]);
+
+function getAddr(name, callback) {
+    var node = namehash(name);
+    // get a resolver address for that name
+    ens.resolver(node, (err, resolverAddress) => {
+        if (!err && resolverAddress != 0) {
+            // if you find one, find the addr of that resolver
+            resolverContract.at(resolverAddress).addr(node, (error, result) => {
+                if (!err && result != 0 && callback){
+                    callback(result);
+                }
+            })
+        }
+    })
+}
+
+function getName(address, callback) {
+    var node = namehash(address.toLowerCase().replace('0x','')+'.addr.reverse');
+    // get a resolver address for that name
+    ens.resolver(node, (err, resolverAddress) => {
+        if (!err && resolverAddress != 0) {
+            // if you find one, find the name on that resolver
+            resolverContract.at(resolverAddress).name(node, (error, result) => {
+                if (!err && result != 0 && callback){
+                    callback(result);
+                }
+            })
+        }
+    })
+}
 
 /**
 The address input template, containg the identicon.
@@ -69,6 +118,10 @@ Template['dapp_addressInput'].helpers({
         } else {
             return TAPi18n.__('elements.checksumAlert');
         }
+    },
+    'ensDisplay': function() {
+        return TemplateVar.get('ensName').split('.').slice(0, -1).reverse().join(' ▸ ');
+
     }
 });
 
@@ -79,13 +132,15 @@ Template['dapp_addressInput'].events({
     
     @event input input, change input
     */
-    'input input, change input': function(e, template){
+    'input input': function(e, template){
         var value = e.currentTarget.value.replace(/\s+/g, '');
+        TemplateVar.set(template, 'hasName', false);                
 
         // add 0x
-        if(value.length > 2 && value.indexOf('0x') === -1) {
+        if(value.length > 38 
+            && value.indexOf('0x') === -1 
+            && /^[0-9a-f]+$/.test(value.toLowerCase())) {
             value = '0x'+ value;
-            e.currentTarget.value = value;
         }
 
         if(web3.isAddress(value) || _.isEmpty(value)) {
@@ -94,16 +149,68 @@ Template['dapp_addressInput'].events({
             if(!_.isEmpty(value)) {
                 TemplateVar.set('value', '0x'+ value.replace('0x',''));
                 TemplateVar.set('isChecksum', web3.isChecksumAddress(value));
+                
+                // if an address was added, check if there's a name associated with it
+                getName(value, (name) => {
+                    // Any address can claim to be any name. Double check it!
+                    getAddr(name, (addr) => {
+                        TemplateVar.set(template, 'hasName', true);                
+                        TemplateVar.set(template, 'ensName', name);
+                        TemplateVar.set(template, 'isValid', true);
+                        TemplateVar.set(template, 'isChecksum', true);  
+                        TemplateVar.set(template, 'value', web3.toChecksumAddress(addr));
+                    });
+                })
+
             } else {
                 TemplateVar.set('value', undefined);
                 TemplateVar.set('isChecksum', true);                
             }
 
+            e.currentTarget.value = value;
+
         } else {
+            if (value.slice(-4) !== '.eth') value = value + '.eth';
+
+            TemplateVar.set('hasName', false);                
+
+            var _template = template;
+            var thisInput = e.currentTarget
+            getAddr(value, (addr) => {
+                TemplateVar.set(template, 'hasName', true);                
+                TemplateVar.set(template, 'isValid', true);
+                TemplateVar.set(template, 'isChecksum', true);  
+                TemplateVar.set(template, 'value', web3.toChecksumAddress(addr));
+                TemplateVar.set(template, 'ensName', value);
+                // check name
+                getName(addr, (name) => {
+                    TemplateVar.set(template, 'ensName', name);
+                })
+            });
+            
             TemplateVar.set('isValid', false);
+            TemplateVar.set('isChecksum', false);
             TemplateVar.set('value', undefined);
         }
 
+    },
+    /**
+    Set the address while typing
+    
+    @event input input, change input
+    */
+    'focus input': function(e, template){
+        if (TemplateVar.get('hasName')) e.currentTarget.value = TemplateVar.get('ensName');
+    },
+    /**
+    Set the address while typing
+    
+    @event input input, change input
+    */
+    'blur input': function(e, template){
+        let value = TemplateVar.get('value');
+
+        if (value) e.currentTarget.value = value;
     },
     /**
     Prevent the identicon from beeing clicked.
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ethereum:elements',
   summary: 'Basic elements for Dapps',
-  version: '0.7.12',
+  version: '0.7.13',
   git: 'http://github.com/ethereum/meteor-package-elements'
 });
 
