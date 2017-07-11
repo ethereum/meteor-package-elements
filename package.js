bff97d7bc2a9b0f84ecc37145ca4c3c8f734d991
Everton Fraga <ev@ethereum.org>
2017-07-11 13:52:00 -0300

    Linting file -- trying to fix meteor build issue

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ethereum:elements',
   summary: 'Basic elements for Dapps',
-  version: '0.7.16',
+  version: '0.7.17',
   git: 'http://github.com/ethereum/meteor-package-elements'
 });
 
