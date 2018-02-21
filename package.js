cd34c66fb009e434effc208d6d41f0d370f739f0
Konstantin <e@logvik.com>
2018-02-21 14:54:19 +0300

    Update package.js
    api.use('3stack:bignumber@2.0.0', 'client'); - cost ~400Kb, but web3.js already has it

--- package.js
+++ package.js
@@ -15,10 +15,7 @@ Package.onUse(function(api) {
   api.use('standard-minifiers', 'client');
 
   api.use('alexvandesande:identicon@2.0.2', 'client');
-  api.use('3stack:bignumber@2.0.0', 'client');
-  // api.use('underscorestring:underscore.string@3.1.1', 'client');
-
-  api.use('ethereum:web3@0.15.2', 'client');
+  api.use('ethereum:web3', 'client');
   api.use('ethereum:tools@0.4.0', 'client');
   api.use('frozeman:animation-helper@0.2.5', 'client');
   api.use('frozeman:storage@0.1.8', 'client');
